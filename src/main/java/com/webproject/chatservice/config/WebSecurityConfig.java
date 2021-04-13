09583602dd38fcecb00c4bb753d364e51051ec3d
NamSeok_DT <skatjr30@naver.com>
2021-04-13 23:09:18 +0900

    2021-04-13-23:09 - Test

--- src/main/java/com/webproject/chatservice/config/WebSecurityConfig.java
+++ src/main/java/com/webproject/chatservice/config/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
+        http.cors().configurationSource(corsConfigurationSource());
         http.authorizeRequests()
                 // login 없이 허용
                 .antMatchers("/**").permitAll()
@@ -60,4 +61,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         UsernamePasswordAuthenticationFilter.class);
         // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
     }
+
+    // CORS 허용 적용
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+//        configuration.setAllowCredentials(true);
+        configuration.applyPermitDefaultValues();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
