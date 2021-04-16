63dbde759b2f0c12dfccde08404e0e437d9573ee
NamSeok_DT <skatjr30@naver.com>
2021-04-16 21:23:26 +0900

    2021-04-16-11:43 - 소셜 카카오 로그인 완성 / email 없는 경우 예외처리 해야함

--- src/main/java/com/webproject/chatservice/kakao/KakaoOAuth2.java
+++ src/main/java/com/webproject/chatservice/kakao/KakaoOAuth2.java
@@ -78,8 +78,12 @@ public class KakaoOAuth2 {
 
         JSONObject body = new JSONObject(response.getBody());
         Long id = body.getLong("id");
-        System.out.println("body.getJSONObject(kakao_account) = " + body.getJSONObject("kakao_account"));
-        String email = body.getJSONObject("kakao_account").getString("email");
+        System.out.println("body.getJSONObject(kakao_account).getString(email_needs_argreement) = " + body.getJSONObject("kakao_account").getBoolean("email_needs_agreement"));
+        String email = null;
+        if(!body.getJSONObject("kakao_account").getBoolean("email_needs_agreement"))
+        {
+            email = body.getJSONObject("kakao_account").getString("email");
+        }
         System.out.println("email = " + email);
         String nickname = body.getJSONObject("properties").getString("nickname");
 
